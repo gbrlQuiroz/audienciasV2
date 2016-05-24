bcd4c8aead4c202ca3257974353a95414bff6fbd
antonielle <marytony0606@hotmail.com>
2016-05-24 11:58:47 -0500

    AGREGADOS
    agrege los archivos como corresponde la estructura

---
+++ js/controladores/agendando.js
@@ -0,0 +1,36 @@
+ $(function () {
+     $('#practico').click(function () {
+         var valido = true;
+         $('input').each(function () {
+             var grupo = $(this).parents('.form-group');
+             var icono = grupo.find('.verif');
+             if (this.checkValidity()) {
+                 grupo.addClass('has-success').removeClass('has-error');
+                 icono.addClass('glyphicon-ok').removeClass('glyphicon-remove');
+             } else {
+                 grupo.addClass('has-error').removeClass('has-success');
+                 icono.addClass('glyphicon-remove').removeClass('glyphicon-ok');
+                 valido = false;
+             }
+         });
+         $('select').each(function () {
+             var grupo = $(this).parents('.form-group');
+             var icono = grupo.find('.verif');
+             if (this.checkValidity()) {
+                 grupo.addClass('has-success').removeClass('has-error');
+                 icono.addClass('glyphicon-ok').removeClass('glyphicon-remove');
+             } else {
+                 grupo.addClass('has-error').removeClass('has-success');
+                 icono.addClass('glyphicon-remove').removeClass('glyphicon-ok');
+                 valido = false;
+             }
+         });
+         if (valido) {
+             $('#modaltrue').modal();
+             //$('#success-alert').removeClass();
+         } else {
+             $('#modalfalse').modal();
+             //$('#success_alert').removeClass('hidden');
+         };
+     });
+ });
