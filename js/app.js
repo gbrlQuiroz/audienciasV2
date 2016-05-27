d13207206de495c1ba5a2897beb989ce5adb7eb3
gbrlQuiroz <orion_fire_qog@hotmail.com>
2016-05-27 15:52:50 -0500

    Signed-off-by: gbrlQuiroz <orion_fire_qog@hotmail.com>

--- js/app.js
+++ js/app.js
@@ -1,25 +1,27 @@
 'use strict';
 
 angular
-.module('audienciasV2',[
+    .module('audienciasV2', [
   'ngMessages',
   'ngResource',
   'ngRoute',
   'ngCookies'
 ])
-.config(function($routeProvider){
-  $routeProvider
-  .when('/',{
-    templateUrl:'vistas/acceso.html',
-    controller: 'AccesoCtrl',
-    controllerAs: 'acceso'
-  })
-  .when('/agendando',{
-    templateUrl: 'vistas/agendando.html',
-    controller: 'AgendandoCtrl',
-    controllerAs: 'agendando'
-  })
-  .otherwise({
-    redirectTo:'/'
-  });
-});
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'vistas/acceso.html',
+                controller: 'AccesoCtrl',
+                controllerAs: 'acceso'
+            })
+            .when('/agendando', {
+                templateUrl: 'vistas/agendando.html',
+                controller: 'AgendandoCtrl',
+                controllerAs: 'agendando'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
+
+/*una prueba*/
