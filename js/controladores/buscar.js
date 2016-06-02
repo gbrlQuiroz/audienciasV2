606f1bc94cbb095f8e365d3ad03c27c91bdbb7d9
antonielle <marytony0606@hotmail.com>
2016-06-02 16:26:47 -0500

    seguimos
    Se estan generando los formularios...

---
+++ js/controladores/buscar.js
@@ -0,0 +1,44 @@
+(function () {
+    var app = angular.module('buscar', []);
+
+    app.factory('RESTful', ['$http', function ($http) {
+        var response = {};
+        response.getDatos = function () {
+            return $http.get('api/datos');
+        };
+        response.postDatos = function (datos) {
+            return $http.post('api/datos', datos);
+        };
+        response.getDato = function (id) {
+            var uri = 'api/datos/' + id;
+            return $http.get(uri);
+        };
+        response.putDato = function (id, datos) {
+            var uri = 'api/datos/' + id;
+            return $http.put(uri, datos);
+            //return $http.put(id,datos);
+        };
+        response.getRaro = function () {
+            return $http.get('js/datos.json');
+        };
+        return response;
+        }]);
+
+
+
+    app.controller('meController', ['$scope', 'RESTful', function ($scope, RESTful) {
+
+        /*+++++++++++++++++++++++
+         * 
+         ++++++++++++++++++++++*/
+        cargarRaro();
+
+        function cargarRaro() {
+            RESTful.getRaro()
+                .success(function (datosCargados) {
+                    console.log('------' + datosCargados);
+                    $scope.datos = datosCargados;
+                });
+        };
+    }]);
+})();
